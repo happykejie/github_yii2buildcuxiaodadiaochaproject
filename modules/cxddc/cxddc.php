288e675964b9ccb0bdf8e21c92cc43945e305e85
zhangjie <happykejie@126.com>
2016-09-07 22:37:48 +0800

    9.7 p10
    9.7 p10

---
+++ modules/cxddc/cxddc.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\modules\cxddc;
+
+class cxddc extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\cxddc\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+    }
+}
